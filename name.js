dbe1886562c5ce7af83f99e1bd20a856e88cd583
abdusfarhan <91369611+abdusfarhan@users.noreply.github.com>
2021-10-02 04:28:37 -0700

    Add files via upload

---
+++ name.js
@@ -0,0 +1,9 @@
+function img1(){
+  document.getElementById('audio').play()
+}
+function img2(){
+  document.getElementById('audio1').play()
+}
+function img3(){
+    document.getElementById('audio2').play()
+  }
